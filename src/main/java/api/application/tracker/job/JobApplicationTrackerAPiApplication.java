b8d7067869b14b3d9a0600ff8d00fd181ade8612
Thomas Alemayehu <thomas2alexmech@gmail.com>
2023-09-27 11:53:28 -0500

    Project init

---
+++ src/main/java/api/application/tracker/job/JobApplicationTrackerAPiApplication.java
@@ -0,0 +1,13 @@
+package api.application.tracker.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationTrackerAPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/application/tracker/job/JobApplicationTrackerAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.application.tracker.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackerAPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
